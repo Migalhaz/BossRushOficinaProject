3a37e70e6a9fff41085460a292d07606146bcf84
Raphael César Queiroz do Nascimento <queiroz.raphael@academico.ifrn.edu.br>
2023-09-18 09:54:47 -0300

    health system and boss attack change

---
+++ BossRushProject/Assets/Scripts/Raphael/bossHealthRaphael.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossHealthRaphael : MonoBehaviour, IDie
+{
+
+    public float health;
+
+    public bossAttackRaphael attackBoss;
+    private float maxHealth;
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+        if(health <= maxHealth * 0.5f)
+        {
+            attackBoss.estadoBoss = EstadosBossRaphael.Estado2;
+        }
+        if(health <= 0)
+        {
+            Death();
+        }
+
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
