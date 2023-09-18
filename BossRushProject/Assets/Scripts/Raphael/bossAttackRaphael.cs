95782723be5d5437f76d3021b62bcb62f7f18242
Raphael César Queiroz do Nascimento <queiroz.raphael@academico.ifrn.edu.br>
2023-09-18 16:10:34 -0300

    boss aim

--- BossRushProject/Assets/Scripts/Raphael/bossAttackRaphael.cs
+++ BossRushProject/Assets/Scripts/Raphael/bossAttackRaphael.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class bossAttackRaphael : MonoBehaviour
 {
     public EstadosBossRaphael estadoBoss;
-    // Start is called before the first frame update
+    private Transform playerTransform;
+    public Transform aim;
+
+    [Header("Configurações Estado 1")]
+    public float timeForFirstAttack;
+    public int shots;
+    public float angleShotBreak;
+    public float attackCooldown;
+    public float waveAttackCooldown;
+    private float waveCooldownTimer;
+    public int waveCounter;
+
+    //[Header("Configurações Estado 2")]
+
+
     void Start()
     {
-        
+        playerTransform = PlayerManager.Instance.transform;
     }
 
     // Update is called once per frame
@@ -27,11 +41,48 @@ public class bossAttackRaphael : MonoBehaviour
 
     void UpdateEstadoUm()
     {
-        Debug.Log("update no estado 1");
+        Debug.Log("update no estado 2");
     }
     void UpdateEstadoDois()
     {
         Debug.Log("update no estado 2");
+        LookToPlayer();
+    }
+
+    void LookToPlayer()
+    {
+        Vector3 aimDirection =  playerTransform.position - transform.position;
+
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+
+        aim.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+    [ContextMenu("paulo")]
+    void Teste()
+    {
+        StartCoroutine(FirstStateAttack());
+    }
+
+    IEnumerator FirstStateAttack()
+    {
+        for (int i = 0; i < waveCounter; i++)
+        {
+            int direction = 1;
+
+            if (Random.value <= 0.5f)
+            {
+                direction = -1;
+            }
+
+            for (int j = 0; j < shots; j++)
+            {
+                aim.rotation = Quaternion.Euler(0f, 0f, angleShotBreak * j * direction);
+                yield return new WaitForSeconds(timeForFirstAttack);
+            }
+            yield return new WaitForSeconds(attackCooldown);
+        }
+        
     }
 }
 public enum EstadosBossRaphael
