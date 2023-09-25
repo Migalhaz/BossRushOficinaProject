dd4b97269cf73797fef668ce513e4bd060918846
Bruna Mirelly Ferreira Paixão <20211170150070@ifrn.local>
2023-09-25 10:28:57 -0300

    Boss Logica Finalizada

---
+++ BossRushProject/Assets/Scripts/Bruna/BalaBruna.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaBruna : MonoBehaviour
+{
+    private Transform PlayerTransform;
+    public float VelocidadeDaBala;
+    public float TempoDeVida;
+    public int DanoDoTiro;
+    void Start()
+    {
+        PlayerTransform = PlayerManager.Instance.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MirarNoPlayer();
+        transform.Translate(  VelocidadeDaBala * Time.deltaTime * Vector3.right);
+        TempoDeVida -= Time.deltaTime;
+        if (TempoDeVida <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void MirarNoPlayer()
+    {
+        Vector3 DirecaoDoTiro =  PlayerTransform.position - transform.position;
+
+        float AnguloDoTiro = Mathf.Atan2(DirecaoDoTiro.y, DirecaoDoTiro.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, AnguloDoTiro );
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            col.GetComponent<IDamage>().Damage(DanoDoTiro);
+            Destroy(gameObject);
+        }
+    }
+}
--- BossRushProject/Assets/Scripts/Bruna/EstadoBossBruna.cs
+++ BossRushProject/Assets/Scripts/Bruna/EstadoBossBruna.cs
@@ -4,17 +4,79 @@ using UnityEngine;
 
 public class EstadoBossBruna : MonoBehaviour
 {
+    private Transform playerTransform;
+    public Transform aim;
     public EstadosBossBruna EstadoBoss;
-    // Start is called before the first frame update
+    private Transform FirePoint;
+
+    public GameObject Tiro;
+    [Header("Estado1")]
+    public float TempoParaAtirar;
+    private float TempoAtualDoTiro;
+    
+    [Header("Estado2")]
+    public List<Transform> PontoDeTeleporte;
+
+    public float TempoDeTeleporte;
+    private float TempoDeTeleporteAtual;
     void Start()
     {
-        
+        playerTransform = PlayerManager.Instance.transform;
+        FirePoint = aim.GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
+    { 
+        UpdateEstado1();
+
+        if (EstadoBoss==EstadosBossBruna.Estado2)
+        {
+          UpdateEstado2();  
+        }
+    }
+
+    void MirarNoPlayer()
     {
-        
+        Vector3 DirecaoDoTiro =  playerTransform.position - transform.position;
+
+        float AnguloDoTiro = Mathf.Atan2(DirecaoDoTiro.y, DirecaoDoTiro.x) * Mathf.Rad2Deg;
+
+        aim.rotation = Quaternion.Euler(0f, 0f, AnguloDoTiro );
+    }
+
+    void UpdateEstado1()
+    {
+        MirarNoPlayer();
+        if (TempoAtualDoTiro>=TempoParaAtirar)
+        {
+            TempoAtualDoTiro = 0;
+            Instantiate(Tiro, FirePoint.position, Quaternion.identity);
+        }
+        else
+        {
+            TempoAtualDoTiro += Time.deltaTime;
+        }
+    }
+
+    void UpdateEstado2()
+    {
+        if (TempoDeTeleporteAtual>=TempoDeTeleporte)
+        {
+            TempoDeTeleporteAtual = 0;
+            Teleportar();
+        }
+        else
+        {
+            TempoDeTeleporteAtual += Time.deltaTime;
+        }
+    }
+
+    void Teleportar()
+    {
+        int IndexPonto= Random.Range(0,PontoDeTeleporte.Count);
+        Transform PontoTeleporte = PontoDeTeleporte[IndexPonto];
+        transform.position = PontoTeleporte.position;
     }
 }
 
